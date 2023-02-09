555f1e6fca7cc0c9b24307fe14002cc9ad55e1cb
Petr Reshetin <preshetin@gmail.com>
2023-02-09 22:16:01 +0500

    json response

--- api/index.py
+++ api/index.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
+    result =  {
+        "message": "Work in progress",
+        "distance": 600000
+    }
+    return jsonify(result)
     return 'Hello, World bro'
 
 @app.route('/about')
